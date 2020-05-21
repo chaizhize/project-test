5a93b5c79450521b7e597730ebb231eea97cafd6
chaizhize <chaizhize>
2020-05-21 16:09:58 +0800

    process

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ if [ -d ./dist ];then
 	echo "removing dist dir"
 	rm -rf ./dist
 fi
-npm install
 npm run build:prod
 echo 'success'
 
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import 'normalize.css';
 import './plugins';
 Vue.config.productionTip = false; //阻止启动生产消息，常用作指令
 Vue.prototype.$bus = new Vue(); // event Bus 用于无关系组件间的通信。
-
+console.log(process.env.NODE_ENV, 'xxx');
 new Vue({
     router,
     store,
