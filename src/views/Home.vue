66f3a09a844bef2bb4cc26b733e2a2dfae0fe644
chaizhize <chaizhize>
2020-05-21 17:46:58 +0800

    msg

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
         <img alt="Vue logo" src="../assets/logo.png" />
         <HelloWorld msg="111111111111111111111Welcome to Your Vue.js App22222222222222222222222" />
         <div>33333333333333333333333</div>
+        <div>{{ msg }}</div>
     </div>
 </template>
 
@@ -13,6 +14,11 @@ import HelloWorld from '@/components/HelloWorld.vue';
 export default {
     components: {
         HelloWorld
+    },
+    data() {
+        return {
+            msg: process.env.NODE_ENV
+        };
     },
     mounted() {}
 };
