4668e69ed36b91eab25f337cd03a47d881fb855e
chaizhize <chaizhize>
2020-11-04 19:37:49 +0800

    1111111

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-11-04 19:37:24
+ * @LastEditTime: 2020-11-04 19:37:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vue-cli3/src/App.vue
+-->
 <template>
     <div id="app">
+        11111111111111111111
         <div id="nav">
+            
             <el-button type="primary"><router-link to="/">home哈哈哈哈哈11111111111111111111</router-link></el-button>
             <el-button type="success"><router-link to="/about">About</router-link></el-button>
         </div>
